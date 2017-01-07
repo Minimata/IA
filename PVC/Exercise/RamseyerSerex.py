a5af86e4c3dde430a5cee4ce8c37733f9b571f1a
Alexandre Serex <alexandre.serex@he-arc.ch>
2017-01-07 15:48:02 +0100

    everything made ready for implementing the REAL SHIT NIGGA

--- PVC/Exercise/RamseyerSerex.py
+++ PVC/Exercise/RamseyerSerex.py
@@ -1,25 +1,9 @@
 import pygame
 from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
-import sys, argparse
+import sys, argparse, csv
 
 
-# INIT
-screen_x = screen_y = 500
-
-city_color = [10, 10, 200]  # blue
-city_radius = 3
-
-font_color = [255, 255, 255]  # white
-
-pygame.init()
-window = pygame.display.set_mode((screen_x, screen_y))
-pygame.display.set_caption('Exemple')
-screen = pygame.display.get_surface()
-font = pygame.font.Font(None, 30)
-
-end = False
-
-
+# METHOD DEFINITIONS
 def draw(positions):
 	screen.fill(0)
 	for pos in positions:
@@ -39,27 +23,68 @@ def draw_itinerary(text, cities):
 	pygame.display.flip()
 
 
-def generate_itinerary(cities):
+
+def parse_filename(filename):
+	cities = {}
+	if filename:
+		with open(filename, newline='') as f:
+			reader = csv.reader(f, delimiter=" ")
+			for name, x, y in reader:
+				cities[name] = (int(x), int(y))
 	return cities
 
 
+def ga_solve(file=None, gui=True, maxtime=0):
+	pass
+
+
+
+
+# ARGS PARSING
+parser = argparse.ArgumentParser(description='PVC Genetic solver')
+parser.add_argument('--nogui', default=False, action='store_true')
+parser.add_argument('--maxtime', default=0, type=int)
+parser.add_argument('filename', nargs='?', default=None)
+args = parser.parse_args()
+
+nogui, maxtime, filename = [vars(args).get(k) for k in ['nogui', 'maxtime', 'filename']]
+
+# INIT
+screen_x = screen_y = 500
+
+city_color = [10, 10, 200]  # blue
+city_radius = 3
+
+font_color = [255, 255, 255]  # white
+
+pygame.init()
+window = pygame.display.set_mode((screen_x, screen_y))
+pygame.display.set_caption('Exemple')
+screen = pygame.display.get_surface()
+font = pygame.font.Font(None, 30)
+
+end = False
+
+# LOOP
 while not end:
-	cities = []
-	draw(cities)
+	cities = parse_filename(filename)  # if filename fill cities
+	draw(list(cities.values()))
 
 	collecting = True
-	while collecting:
+	id_count = 0
+	while collecting and not filename:
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				sys.exit(0)
 			elif event.type == KEYDOWN and event.key == K_RETURN:
 				collecting = False
 			elif event.type == MOUSEBUTTONDOWN:
-				cities.append(pygame.mouse.get_pos())
-				draw(cities)
+				cities[id_count] = (pygame.mouse.get_pos())
+				id_count += 1
+				draw(list(cities.values()))
 
-	itinerary = generate_itinerary(cities)
-	draw_itinerary("Un chemin", cities)
+	ga_solve(filename, not nogui, maxtime)
+	draw_itinerary("Un chemin", list(cities.values()))
 
 	while True:
 		event = pygame.event.wait()
@@ -72,32 +97,4 @@ while not end:
 				break
 
 
-def ga_solve(file=None, gui=True, maxtime=0):
-	pass
 
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='PVC Genetic solver')
-	parser.add_argument('--nogui', default=False, action='store_true')
-	parser.add_argument('--maxtime', default=0, type=int)
-	parser.add_argument('filename', default=None)
-	args = parser.parse_args()
-
-	print(args)
-
-	# inputfile = ''
-   # outputfile = ''
-   # try:
-   #    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   # except getopt.GetoptError:
-   #    print 'test.py -i <inputfile> -o <outputfile>'
-   #    sys.exit(2)
-   # for opt, arg in opts:
-   #    if opt == '-h':
-   #       print 'test.py -i <inputfile> -o <outputfile>'
-   #       sys.exit()
-   #    elif opt in ("-i", "--ifile"):
-   #       inputfile = arg
-   #    elif opt in ("-o", "--ofile"):
-   #       outputfile = arg
-   # print 'Input file is "', inputfile
-   # print 'Output file is "', outputfile
